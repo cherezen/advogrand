4ecfff403b4e9d4c4423966cc09dddc64e42dbce
cherezen <61204687+cherezen@users.noreply.github.com>
2021-01-13 17:01:40 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,34 +17,34 @@ const tabs = document.querySelectorAll('.feedback__tab'),
 
 // ******************** ТАБЫ ВИДЕО ***********************//
 
-function hideTabContent() {
-    tabsContent.forEach(item => {
-        item.style.display = 'none';
-    });
-    tabs.forEach(item => {
-        item.classList.remove('feedback__tab_active');
-    })
-}
+// function hideTabContent() {
+//     tabsContent.forEach(item => {
+//         item.style.display = 'none';
+//     });
+//     tabs.forEach(item => {
+//         item.classList.remove('feedback__tab_active');
+//     })
+// }
 
-function showTabContent(i = 0) {
-    tabsContent[i].style.display = 'block';
-    tabs[i].classList.add('feedback__tab_active');
-}
+// function showTabContent(i = 0) {
+//     tabsContent[i].style.display = 'block';
+//     tabs[i].classList.add('feedback__tab_active');
+// }
 
-hideTabContent();
-showTabContent();
+// hideTabContent();
+// showTabContent();
 
-tabsParent.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target && target.classList.contains('feedback__tab')) {
-        tabs.forEach((item, i) => {
-            if (target == item) {
-                hideTabContent();
-                showTabContent(i);
-            }
-        })
-    }
-});
+// tabsParent.addEventListener('click', (event) => {
+//     const target = event.target;
+//     if (target && target.classList.contains('feedback__tab')) {
+//         tabs.forEach((item, i) => {
+//             if (target == item) {
+//                 hideTabContent();
+//                 showTabContent(i);
+//             }
+//         })
+//     }
+// });
 
 
 // ************************ ТАБЫ ТАРИФЫ *************************//
